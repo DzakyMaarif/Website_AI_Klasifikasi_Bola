961c98c7f3d2911e6a3d1c802f27eb38c8200a46
DzakyMaarif <mohammaddzakymaarif@gmail.com>
2025-07-14 20:16:17 +0700

    ganti endpoint

--- laravel-app/app/Http/Controllers/BallClassifierController.php
+++ laravel-app/app/Http/Controllers/BallClassifierController.php
@@ -26,7 +26,7 @@ class BallClassifierController extends Controller
         // Kirim ke API Python
         $response = Http::attach(
             'image', file_get_contents($image), $image->getClientOriginalName()
-        )->post('http://192.168.43.152:5000/predict');
+        )->post('http://localhost:5000/api/predict');
 
         if ($response->successful()) {
             $result = $response->json();
